2242a65254f817924f453a778f3a82f57da76117
bierbaumtim <tb2706@outlook.de>
2023-03-25 19:24:40 +0100

    update application windows title

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"arb_editor", origin, size)) {
+  if (!window.Create(L"Arb Editor", origin, size))
+  {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
